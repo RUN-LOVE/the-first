6fb1721c49ce9b7010b4d0ba97e03d8bae9bc13b
RUN-LOVE <1099791723@qq.com>
2025-03-30 17:55:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-function processForm() {
-  // 获取用户输入的性别和年龄
-  let gender = document.getElementById('gender').value;
-  let age = parseInt(document.getElementById('age').value);
-
-  // 判断性别和年龄并显示对应的问候信息
-  let greeting = '';
-  if (gender === 'male') {
-    greeting = (age >= 20) ? '小伙子，谢谢你的提交！' : '小兄弟，谢谢你的提交！';
-  } else if (gender === 'female') {
-    greeting = (age >= 20) ? '女士，谢谢你的提交！' : '小姑娘，谢谢你的提交！';
+const button = document.getElementById("add");  // 选择按钮
+button.addEventListener("click", function () {        // 添加点击事件监听
+  let num1 = document.getElementById('num1').value;
+  let num2 = document.getElementById('num2').value;
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+  if (isNaN(num1) || isNaN(num2)) {
+    console.error("请输入有效的数字");
   } else {
-    greeting = '谢谢你的提交！';
+    alert(num1 + num2);
   }
-
-  // 显示问候信息到页面上
-  document.getElementById('greetingMessage').innerText = greeting;
-
-  // 在控制台中显示问候信息
-  console.log(greeting);
-}
+});
